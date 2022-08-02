3597000daabf6e61159c3a5876fcd27ea647579e
Jian Kim <jian7061@gmail.com>
2022-08-02 13:28:05 +0900

    wrap _app.tsx with Provider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,21 @@
-import { AppProps } from "next/app"
-import React from "react"
-
+import { AppProps } from "next/app";
+import React from "react";
+import { Provider } from "starknet";
+import {
+  getInstalledInjectedConnectors,
+  StarknetProvider,
+} from "@starknet-react/core";
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  const connectors = getInstalledInjectedConnectors();
+
+  return (
+    <StarknetProvider
+      connectors={connectors}
+      defaultProvider={new Provider({ baseUrl: "http://localhost:5050" })}
+    >
+      <Component {...pageProps} />
+    </StarknetProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,11 @@
 import type { NextPage } from "next";
 import { ConnectWallet } from "~/components/ConnectWallet";
-import {
-  getInstalledInjectedConnectors,
-  StarknetProvider,
-} from "@starknet-react/core";
-import { Provider } from "starknet";
-const Home: NextPage = () => {
-  const connectors = getInstalledInjectedConnectors();
 
+const Home: NextPage = () => {
   return (
     <div>
-      <StarknetProvider
-        connectors={connectors}
-        defaultProvider={new Provider({ baseUrl: "http://localhost:5050" })}
-      >
-        <h2>Wallet</h2>
-        <ConnectWallet />
-      </StarknetProvider>
+      <h2>Wallet</h2>
+      <ConnectWallet />
     </div>
   );
 };
