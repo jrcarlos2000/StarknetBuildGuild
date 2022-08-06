21609b151d1483d708da1ebe4e0fbef7393626c8
Jian Kim <jian7061@gmail.com>
2022-08-06 14:13:45 +0900

    Add ReadMe component

---
+++ frontend/src/components/ReadMe.tsx
@@ -0,0 +1,27 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+export default function ReadMe({ url }: any) {
+  const [repo, setRepo] = useState<any>();
+  useEffect(() => {
+    const getRepo = async () => {
+      try {
+        const repo = await fetch(url);
+        setRepo(await repo.text());
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getRepo();
+  }, []);
+
+  return (
+    <Wrapper>
+      <ReactMarkdown children={repo} remarkPlugins={[remarkGfm]} />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div``;
