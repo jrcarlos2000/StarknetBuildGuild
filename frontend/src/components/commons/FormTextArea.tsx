658558849c1db8db6aa41f9710c785404a2fe409
chen <23054115+cwkang1998@users.noreply.github.com>
2022-08-09 03:11:17 +0900

    feat: extract modals into its own and update styling for all modals

---
+++ frontend/src/components/commons/FormTextArea.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FormTextArea = styled.textarea`
+  padding: 0.75rem;
+  border-radius: 0.5rem;
+  border: 1px solid #cccccc;
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  ::placeholder {
+    color: #9a9b9c;
+  }
+`;
