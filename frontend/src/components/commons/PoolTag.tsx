dbd974519a9c9d45a3f0c85a5086aeedb0a0c29c
Jian Kim <jian7061@gmail.com>
2022-08-10 14:41:43 +0900

    add pool tag

---
+++ frontend/src/components/commons/PoolTag.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PoolTag = styled.button`
+  color: #ffffff;
+  font-size: 0.6rem;
+  background-color: #fe4a04;
+  border-radius: 10px;
+  border: none;
+  text-align: center;
+  padding: 0 0.3rem;
+  &:hover {
+    cursor: pointer;
+    background-color: #ffffff;
+    color: #fe4a04;
+    border: 1px solid #fe4a04;
+  }
+`;
